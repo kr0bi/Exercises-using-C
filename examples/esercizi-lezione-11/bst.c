bebcb78e8d424d6836c6e3c33c623a27346b0f49
Kr0bi <krobi@cock.li>
2019-04-05 01:42:09 +0200

    introduzione della creazione della funzione to_list

--- examples/esercizi-lezione-11/bst.c
+++ examples/esercizi-lezione-11/bst.c
@@ -162,6 +162,96 @@ void destroy (struct node *T){
     }
 }
 
+//INTRODUZIONE DI UNA LISTA CONCATENATA
+
+struct list{
+    int data;
+    struct list *next;
+};
+
+struct list *create(int data){
+    struct list *ptr = malloc(sizeof(struct list));
+    ptr->data = data;
+    ptr->next = NULL;
+    return ptr;
+}
+
+int length (struct list *head){
+    int len = 0;
+    for (struct list *n = head; n; n = n->next){
+        ++len;
+    }
+    return len;
+}
+
+struct list *find (struct list *head, int data){
+    for (struct list *n = head; n; n = n->next){
+        if (n->data == data){
+            return n;
+        }
+    }
+    return NULL;
+}
+
+struct list *last (struct list *head){
+    for (struct list *n = head; n; n = n->next){
+        if (n->next == NULL){
+            return n;
+        }
+    }
+    return NULL;
+}
+
+struct list *append (struct list *head1, struct list *head2){
+    struct list *last1 = last (head1);
+    last1->next = head2;
+    return head1;
+}
+
+struct list *add (struct list *head, int data){
+    struct list *datoDaAggiungere = create(data);
+    struct list *combined = append(head, datoDaAggiungere);
+    return combined;
+}
+
+void destroy (struct list *head){
+    struct list *next = head;
+    while (next){
+        struct list *n = next;
+        next = n->next;
+        free(n);
+    }
+}
+
+void viewlist (struct list *head){
+    for (struct list *n = head; n; n = n->next){
+        printf("%d ", n->data);
+    }
+    printf("\n");
+}
+
+//FINE INTRODUZIONE
+
+/*
+    funzione che dato un bst ritorna una lista concatenata
+
+struct list *to_list(struct node *T){
+    struct list *ptr = NULL;
+    if (T!=NULL){
+        to_list_rec(x->left, ptr->next);
+        create()
+        inorder_tree_walk_rec(x->right);
+    }
+}
+
+struct list *to_list_rec(struct node *T, struct list *lst){
+    
+}
+
+*/
+
+
+
 /*
     test del bst
 */
