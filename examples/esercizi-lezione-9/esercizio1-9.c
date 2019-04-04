fb4aad32478032bfb4b2e15b5b3ec6ecab1f5ad0
Kr0bi <krobi@cock.li>
2019-04-05 01:56:17 +0200

    cancellazione dei file spostati

--- examples/esercizi-lezione-9/esercizio1-9.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (){
-    int x = 0, y=0, z=0;
-    printf("Inserisci tre numeri interi: ");
-    scanf("%d%d%d", &x, &y, &z);
-    printf("La somma di %d, %d e %d e': %d\n", x, y, z, x+y+z);
-    return 0;
-}
